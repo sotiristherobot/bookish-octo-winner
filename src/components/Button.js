fad84c279f78e325d720785fb51c9f01f01fa364
Sotiris Ioannou <sotiris@jsmate.net>
2020-06-08 22:39:49 +0200

    Reusable Button.js

---
+++ src/components/Button.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const StyledButton = styled.button`
+  padding: 10px;
+  border: 1px solid;
+  background: white;
+`;
+
+/**
+ * Reusable button component
+ * @component
+ * @param {function} handleClick Click handler for the button
+ * @param {string} type Type of the button, by default is set to submit
+ * @param {string} label What should be used as a label for the button by default is set to Submit
+ */
+const Button = ({ handleClick, type = "submit", label = "Submit" }) => (
+  <StyledButton type={type} onClick={handleClick}>
+    {label}
+  </StyledButton>
+);
+export default Button;
+
+Button.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  type: PropTypes.string,
+  label: PropTypes.string,
+};
