d925e4485f37365f4adc999521b4df05ba5c3d4d
Sotiris Ioannou <sotiris@jsmate.net>
2020-06-09 11:28:04 +0200

    useTemperature.js hook that returns the current temperature for the provided city

---
+++ src/hooks/useTemperature.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+/**
+ * Reusable hook that returns the current temperature for the provided city
+ * The isLoading @param is provided in case the component that utilizes this hook,
+ * needs to display some loading logic while the data are being fetched
+ * @param {string} city
+ * @returns {[boolean, string]}
+ */
+const useTemperature = (city) => {
+  const [isLoading, setIsloading] = React.useState(true);
+  const [temp, setTemp] = React.useState(null);
+
+  React.useEffect(() => {
+    setIsloading(true);
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=23a5a3bad50e85f575448196aaf5af0a`,
+      {
+        method: "GET",
+      }
+    )
+      .then((res) => res.json())
+      .then(({ main: { temp } }) => setTemp(temp));
+  }, []);
+
+  return [isLoading, temp];
+};
+export default useTemperature;
+useTemperature.propTypes = {
+  city: PropTypes.string.isRequired,
+};
